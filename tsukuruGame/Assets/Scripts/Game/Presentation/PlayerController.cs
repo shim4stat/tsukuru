43eee2149a8bc4fe693e0ff6804d7452474264be
shim4stat <s234510v@st.go.tuat.ac.jp>
2026-02-18 11:22:23 +0900

    PlayerControllerを仮実装

---
+++ tsukuruGame/Assets/Scripts/Game/Presentation/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Game.Domain.Battle.Player _domainPlayer; // Domain層のPlayerへの参照
+
+    // 初期化時にDomainのPlayerを受け取る
+    public void Initialize(Game.Domain.Battle.Player player)
+    {
+        _domainPlayer = player;
+    }
+
+    void Update()
+    {
+        if (_domainPlayer != null)
+        {
+            transform.position = new Vector3(_domainPlayer.Position.X, _domainPlayer.Position.Y, 0);
+        }
+    }
+}
